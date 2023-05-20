cec1287d6133011b1c499d0807f3e166f58de2c0
dangha <dangha997@gmail.com>
2023-05-20 16:37:59 +0200

    fix the directory

--- ex03/template/run_via_sbatch.sh
+++ ex03/template/run_via_sbatch.sh
@@ -14,12 +14,12 @@ conda activate EML_ex03
 
 echo "Running Task 1: "
 echo "CPU runing"
-python3 template/exercise03_template.py --dataset mnist --model mlp --epochs 30 --no-cuda --output-file "task01_cpu.json"
+python3 exercise03_template.py --dataset mnist --model mlp --epochs 30 --no-cuda --output-file "task01_cpu.json"
 echo "GPU runing"
-python3 template/exercise03_template.py --dataset mnist --model mlp --epochs 30 --output-file "task01_gpu.json"
+python3 exercise03_template.py --dataset mnist --model mlp --epochs 30 --output-file "task01_gpu.json"
 
 echo "Running Task 2: "
 echo "MLP runing"
-python3 template/exercise03_template.py --dataset svhn --model mlp --epochs 30 --output-file "task02_mlp.json"
+python3 exercise03_template.py --dataset svhn --model mlp --epochs 30 --output-file "task02_mlp.json"
 echo "CNN runing"
-python3 template/exercise03_template.py --dataset svhn --model cnn --lr 0.01 --epochs 30 --output-file "task02_cnn.json"
+python3 exercise03_template.py --dataset svhn --model cnn --lr 0.01 --epochs 30 --output-file "task02_cnn.json"
