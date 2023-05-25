68b954af5cb8685611cb985bdce423dcde5e1b55
Damjan Kalšan <damjankalsan@gmail.com>
2023-05-25 21:04:58 +0200

    Added experiments

--- ex04/template/run_via_sbatch.sh
+++ ex04/template/run_via_sbatch.sh
@@ -12,5 +12,23 @@ source ~/.bashrc
 conda activate EML_ex03
 
 echo "Running Task 1: "
+
 echo "Base training"
 python3 exercise04_template.py --epochs 30 --output-file "task01_base.json"
+
+echo "CPU Run"
+python3 exercise04_template.py --epochs 1 --no-cuda
+
+echo "GPU Run"
+python3 exercise04_template.py --epochs 1
+
+echo "Dropout 0.1"
+python3 exercise04_template.py --epochs 30 --dropout_p 0.1 --output-file "task01_dropout01.json"
+
+echo "Dropout 0.4"
+python3 exercise04_template.py --epochs 30 --dropout_p 0.4 --output-file "task01_dropout04.json"
+
+echo "Dropout 0.7"
+python3 exercise04_template.py --epochs 30 --dropout_p 0.7 --output-file "task01_dropout07.json"
+
+echo "---------------------------------"
