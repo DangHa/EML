dc829e1d31e26a45d2e51ab8158a5c318f9c8dce
Damjan Kalšan <damjankalsan@gmail.com>
2023-06-12 22:09:14 +0200

    Added slurm jobs

---
+++ ex05/template/task02-no-norm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --gres gpu:1
+#SBATCH --mem 10G
+#SBATCH --cpus-per-task 2
+#SBATCH --time 00:30:00
+#SBATCH -p exercise
+#SBATCH -o slurm_output.log
+
+echo "Loading conda env"
+module load anaconda/3
+source ~/.bashrc
+conda activate EML_ex03
+
+echo "Running exercise05_template.py"
+
+echo "ResNet training"
+python3 exercise05_template.py --epochs 50 --output-file "task01_resnet.json"
